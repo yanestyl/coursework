2befc5c5f7a9b621ad9e74a6b8408a8a2035f9df
yanestyl <100021388+yanestyl@users.noreply.github.com>
2022-04-15 11:13:18 +0300

    Create SoftwareDefinitions.h

---
+++ SoftwareDefinitions.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define OnReloadSoftware	1
+#define OnSaveFile			2
+#define OnExitSoftware		3
+
+#define OnAboutIt			4
+
+#define OnDelet				5
+
+
+#define TextBufferSize		1024
+
+char Buffer[TextBufferSize];
+int CharsRead;
+
+
+
+HWND hListBox;
+
+LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
+
+void MainWndAddMenues(HWND hWnd);
+void MainWndAddWidgets(HWND hWnd);
+void MainWndStart(HWND hWnd);
+void SaveDate(LPCSTR path);
+void DeleteApp();
