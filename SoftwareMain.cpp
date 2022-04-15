a8f9a652c965033e536e68d5aab5b1a78faa054c
yanestyl <100021388+yanestyl@users.noreply.github.com>
2022-04-15 11:12:27 +0300

    Create SoftwareMain.cpp

---
+++ SoftwareMain.cpp
@@ -0,0 +1,186 @@
+#include <Windows.h>
+#include <string>
+#include <Winuser.h>
+#include <clocale>
+#include "resource.h"
+#include "SoftwareDefinitions.h"
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+
+	WNDCLASS SoftwareMainClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)),
+	L"MainWndClass", SoftwareMainProcedure);
+	
+	if (!RegisterClassW(&SoftwareMainClass)) { return -1; }
+
+	MSG SoftwareMainMessage = { 0 };
+
+	CreateWindow(L"MainWndClass", L"application destroyer", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 900, 550, NULL, NULL, NULL, NULL);
+	while (GetMessage(&SoftwareMainMessage, NULL, NULL, NULL)) {
+		TranslateMessage(&SoftwareMainMessage);
+		DispatchMessage(&SoftwareMainMessage);
+	}
+	return 0;
+
+}
+
+WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
+	WNDCLASS NWC = { 0 };
+
+	NWC.hIcon = Icon;
+	NWC.hCursor = Cursor;
+	NWC.hInstance = hInst;
+	NWC.lpszClassName = Name;
+	NWC.hbrBackground = BGColor;
+	NWC.lpfnWndProc = Procedure;
+
+	return NWC;
+}
+
+// - - - - - - - - - - О Б Р А Б О Т Ч И К - С О Б Ы Т И Й - - - - - - - - - - //
+LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+	case WM_COMMAND:
+		switch (wp) {
+		case OnReloadSoftware:
+			MainWndStart(hWnd);
+			break;
+		case OnSaveFile:
+			SaveDate("C:\\Users\\yaros\\source\\repos\\Project1\\output.txt");
+			break;
+		case OnExitSoftware:
+			PostQuitMessage(0);
+			break;
+		case OnAboutIt:
+
+			break;
+		default: break;
+		}
+		break;
+	case WM_CREATE:
+		MainWndAddMenues(hWnd);
+		MainWndStart(hWnd);
+		MainWndAddWidgets(hWnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default: return DefWindowProc(hWnd, msg, wp, lp);
+	}
+}
+
+
+// - - - - - - - - - - М Е Н Ю - - - - - - - - - - //
+void MainWndAddMenues(HWND hWnd) {
+	
+	HMENU RootMenu = CreateMenu();
+
+	HMENU SubMenu1 = CreateMenu();
+	HMENU SubMenu2 = CreateMenu();
+
+	AppendMenu(SubMenu1, MF_STRING, OnReloadSoftware, L"Reload");
+	AppendMenu(SubMenu1, MF_STRING, OnSaveFile, L"Save");
+	AppendMenu(SubMenu1, MF_SEPARATOR, NULL, NULL);
+	AppendMenu(SubMenu1, MF_STRING, OnExitSoftware, L"Exit");
+
+	AppendMenu(SubMenu2, MF_STRING, OnAboutIt, L"About it");
+
+	AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)SubMenu1, L"File");
+	AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)SubMenu2, L"Help");
+
+	SetMenu(hWnd, RootMenu);
+}
+
+
+// - - - - - - - - - - В И Д Ж Е Т Ы - - - - - - - - - - //
+void MainWndAddWidgets(HWND hWnd) {
+
+	hListBox = CreateWindowA("LISTBOX", NULL, WS_CHILD | WS_VSCROLL | WS_BORDER | WS_VISIBLE | LBS_SORT | LB_ADDSTRING, 30, 30, 820, 350, hWnd, NULL, NULL, NULL);
+	
+	SendMessage(hListBox, LB_ADDSTRING, 0,
+		(LPARAM)(LPSTR)"ENGLISH");
+
+	CreateWindowA("button", "Save", WS_VISIBLE | WS_CHILD | ES_CENTER, 100, 400, 150, 30, hWnd, (HMENU)OnSaveFile, NULL, NULL);
+	CreateWindowA("button", "Delete", WS_VISIBLE | WS_CHILD | ES_CENTER, 300, 400, 150, 30, hWnd, (HMENU)DeleteApp, NULL, NULL);
+	CreateWindowA("button", "Exit", WS_VISIBLE | WS_CHILD | ES_CENTER, 500, 400, 150, 30, hWnd, (HMENU)OnExitSoftware, NULL, NULL);
+
+}
+
+
+// - - - - - - - - - - С О Х Р А Н Е Н И Е - В - Ф А Й Л - - - - - - - - - - //
+void SaveDate(LPCSTR path) {
+	HANDLE FileToSave = CreateFileA(
+		path,
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	int saveLenth = GetWindowTextLength(hListBox) + 1;
+	char* data = new char[saveLenth];
+
+	saveLenth = GetWindowTextA(hListBox, data, saveLenth);
+
+	DWORD bytesIterated;
+	WriteFile(FileToSave, data, saveLenth, &bytesIterated, NULL);
+
+	CloseHandle(FileToSave);
+	delete[] data;
+}
+
+
+// - - - - - - - - - - У Д А Л Е Н И Е - - - - - - - - - - //
+void DeleteApp() {
+
+	//----------------------------------------------
+
+}
+
+
+// - - - - - - - - - - С Ч И Т Ы В А Н И Е - Р Е Е С Т Р А - - - - - - - - - - //
+void MainWndStart(HWND hWnd) {
+	/*setlocale(LC_CTYPE, "utf8");
+	DWORD dwIndex = 0;
+	LONG ret;
+	DWORD cbName = 256;
+	TCHAR szSubKeyName[256];
+	char szDisplayName[256];
+	DWORD dwSize;
+	DWORD dwType;
+	HKEY hKey;
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_READ, &hKey) != ERROR_SUCCESS)
+		return;
+	else MessageBox(0, 0, 0, 0);
+	// Поочередно проходим по каждому вложенному разделу
+	while ((ret = RegEnumKeyEx(hKey, dwIndex, szSubKeyName, &cbName, NULL, NULL, NULL, NULL)) != ERROR_NO_MORE_ITEMS)
+	{
+		if (ret != ERROR_SUCCESS)
+			continue;
+		// открываем вложенный раздел и ищем в нем ключ DisplayName
+		HKEY hItem;
+		if (RegOpenKeyEx(hKey, szSubKeyName, 0, KEY_READ, &hItem) != ERROR_SUCCESS)
+			continue;
+		else SendMessage(hListBox, LB_ADDSTRING, 0,
+			(LPARAM)(LPSTR)"55555555555555555555");
+		// отображаем на экране название установленной программы
+		dwSize = sizeof(szDisplayName);
+		if (RegQueryValueEx(hItem, L"DisplayName", NULL, &dwType, (LPBYTE)&szDisplayName, &dwSize) == ERROR_SUCCESS)
+		{
+			SendMessage(hListBox, LB_ADDSTRING, 0,
+				(LPARAM)(LPSTR)"111");
+		}
+	
+		//MessageBox(hWnd, (LPCWSTR)'GOOD', NULL, NULL);
+			//SendMessageA(hListBox, (UINT)szDisplayName, LB_ADDSTRING, NULL);
+			//SendMessage(hListBox, (UINT)szDisplayName, NULL, NULL);
+			//AddItem(hListBox, szDisplayName, hbmpitem);
+			//LB_ADDSTRING(&hListBox, szDisplayName);
+		RegCloseKey(hItem);
+		dwIndex++;
+		cbName = 256;
+	}
+	RegCloseKey(hKey);*/
+}
+
+
