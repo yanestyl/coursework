4503d71f6281d70fe98a587864819aaaad89c69c
yanestyl <100021388+yanestyl@users.noreply.github.com>
2022-05-24 11:47:36 +0300

    Update SoftwareDefinitions.h

--- SoftwareDefinitions.h
+++ SoftwareDefinitions.h
@@ -6,7 +6,7 @@
 
 #define OnAboutIt			4
 
-#define OnDelet				5
+#define OnDelete				5
 
 
 #define TextBufferSize		1024
@@ -14,19 +14,21 @@
 char Buffer[TextBufferSize];
 int CharsRead;
 
-
+char ProgramName[256];
+bool uninstall = false;
 
 HWND hListBox;
+HWND mesbox;
 
 LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
 
 void MainWndAddMenues(HWND hWnd);
 void MainWndAddWidgets(HWND hWnd);
-void MainWndStart0(HWND hWnd);
-void MainWndStart(HWND hWnd);
-void MainWndStart1(HWND hWnd);
-void MainWndStart2(HWND hWnd);
-void MainWndStart3(HWND hWnd);
+void MainWndStart(HWND hWnd, bool uninstall, char ProgramName[]);
+void MainWndStart1(HWND hWnd, bool uninstall, char ProgramName[]);
+void MainWndStart2(HWND hWnd, bool uninstall, char ProgramName[]);
+void MainWndStart3(HWND hWnd, bool uninstall, char ProgramName[]);
 void SaveDate(LPCSTR path);
-void DeleteApp();
+void DeleteApp(HWND hWnd);
+void AboutIt(HWND hWnd);
